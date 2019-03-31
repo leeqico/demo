8fb7385b77e329cfb0393a5a8c3fb2c704b4953c
liqc <377028432@qq.com>
2019-03-31 22:40:39 +0800

    增加redis的配置

---
+++ demo02/src/test/java/com/example/demo02/utils/RedisTest.java
@@ -0,0 +1,34 @@
+package com.example.demo02.utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Resource
+    private RedisUtils redisUtils;
+
+    /**
+     * 插入缓存数据
+     */
+    @Test
+    public void set() {
+        redisUtils.set("redis_key", "redis_vale");
+    }
+
+    /**
+     * 读取缓存数据
+     */
+    @Test
+    public void get() {
+        String value = redisUtils.get("redis_key");
+        System.out.println(value);
+    }
+
+}
